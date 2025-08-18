c3ed68729517d83f11d09879ab1ceb3e835f3fc0
Max-Lev <maxlevtov@gmail.com>
2025-08-18 18:42:23 +0300

    shared lib component

--- apps/clients/src/app/app.ts
+++ apps/clients/src/app/app.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { NxWelcome } from './nx-welcome';
+import { SyncBtn } from './../../../../ui-shared/src/lib/sync-btn/sync-btn';
 
 @Component({
-  imports: [NxWelcome, RouterModule],
+  imports: [NxWelcome, RouterModule,SyncBtn],
   selector: 'app-root',
   templateUrl: './app.html',
   styleUrl: './app.css',
---
+++ ui-shared/src/lib/sync-btn/sync-btn.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lib-sync-btn',
+  imports: [],
+  templateUrl: './sync-btn.html',
+  styleUrl: './sync-btn.css',
+  standalone: true,
+})
+export class SyncBtn {
+
+}
